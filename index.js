7b04639978bbfb0dae68ba18ea91f685f001dc87
Matheus Fidelis <msfidelis@outlook.com>
2018-04-05 11:53:59 -0300

    add fields

--- index.js
+++ index.js
@@ -1,48 +1,39 @@
 'use strict';
 
-const APP_PUBLIC_IP_ENDPOINT = "http://169.254.169.254/latest/meta-data/public-ipv4";
-const APP_LOCAL_IP_ENDPOINT = "http://169.254.169.254/latest/meta-data/local-ipv4";
-const APP_ZONE_ENDPOINT = "http://169.254.169.254/latest/meta-data/placement/availability-zone";
-const APP_HOSTNAME_ENDPOINT = "http://169.254.169.254/latest/meta-data/hostname"
+const APP_PUBLIC_IP_ENDPOINT	= "http://169.254.169.254/latest/meta-data/public-ipv4";
+const APP_LOCAL_IP_ENDPOINT		= "http://169.254.169.254/latest/meta-data/local-ipv4";
+const APP_ZONE_ENDPOINT			= "http://169.254.169.254/latest/meta-data/placement/availability-zone";
 
-const exphbs = require('express-handlebars');
-const express = require('express');
-const path = require('path');
-const r2 = require('r2');
+const exphbs	= require('express-handlebars');
+const express	= require('express');
+const path		= require('path');
+const r2		= require('r2');
+const os		= require('os');
 
-let public_ip;
-let local_ip;
-let hostname;
-let zone;
-
+const public_ip;
+const local_ip;
+const zone;
 
 // Find Public IP
 r2.get(APP_PUBLIC_IP_ENDPOINT)
     .text
     .then(response => {
         public_ip = response
-    })
+    });
 
 // Find Local IP
 r2.get(APP_LOCAL_IP_ENDPOINT)
     .text
     .then(response => {
         local_ip = response
-    })
-
-// Find Hostname
-r2.get(APP_HOSTNAME_ENDPOINT)
-    .text
-    .then(response => {
-        hostname = response
-    })
+    });
 
 // Find Zone 
 r2.get(APP_ZONE_ENDPOINT)
     .text
     .then(response => {
         zone = response
-    })
+    });
 
 const app = express();
 
@@ -57,11 +48,15 @@ app.set('view engine', 'handlebars');
  */
 app.get('/', (req, res) => {
 
-    let data = {
+    const data = {
         zone: zone,
         public_ip: public_ip,
         local_ip: local_ip,
-        hostname: hostname
+        hostname: os.hostname(),
+		os: os.type(),
+		release: os.release(),
+		arch: os.arch(),
+		uptime: os.uptime()
     };
 
     res.render('home', data);
@@ -73,11 +68,15 @@ app.get('/', (req, res) => {
  */
 app.get('/whoami', (req, res) => {
 
-    let data = {
+    const data = {
         zone: zone,
         public_ip: public_ip,
         local_ip: local_ip,
-        hostname: hostname
+        hostname: os.hostname(),
+		os: os.type(),
+		release: os.release(),
+		arch: os.arch(),
+		uptime: os.uptime()
     };
 
     res.json(data);
